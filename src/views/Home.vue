652fa14426388d3d895d0707b238e9c9f59e5049
林啸 <3446892005@qq.com>
2019-12-18 09:37:21 +0800

    第一次git提交

---
+++ src/views/Home.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="home">
+    <nav>
+      <div class="y-container">
+        <div class="left">
+          <img src="@/assets/logo.png" alt />
+          <a href>社会招聘</a>
+          <a href>校园招聘</a>
+          <a href>关于大疆</a>
+        </div>
+        <div>
+          <a href>登陆</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="main">
+      <div >
+        <p>未来无所不能</p>
+        <p>THE FUTURE OF POSSIBLE</p>
+      </div>
+    </div>
+    <footer>
+      <div class="y-container">
+        <a href>Copyright @ 2019 DJI 大黄创新 版权所有</a>
+        <a href>为何这么帅 |</a>
+        <a href>赞助我们 |</a>
+        <a href>加入我们 |</a>
+        <a href>腾讯公众号 |</a>
+        <a href>阿里巴巴 |</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  overflow: hidden;
+  nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 70px;
+    background-color: #fff;
+    .y-container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      height: 100%;
+      div {
+        display: flex;
+        align-items: center;
+        img {
+          width: 40px;
+          height: 40px;
+          margin-right: 50px;
+          cursor: pointer;
+          height: 100%;
+        }
+        a {
+          color: #3b3e40;
+          font-size: 14px;
+          margin-right: 34px;
+        }
+        a:hover {
+          color: #44a8f2;
+        }
+      }
+    }
+  }
+  .main {
+    height: 100%;
+    background-size: cover;
+    background-position: 50%;
+    background-image: url(https://we2.djicdn.com/hire/public/img/home-page-banner-2.d8f1ba7.png);
+    font-size: 33px;
+    color: #fff;
+    line-height: 1.5;
+    text-align: center;
+    padding-top: 5 0px;
+    div{
+      padding:200px;
+    }
+  }
+  footer {
+    background-color: #fff;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    height: 40px;
+    line-height: 40px;
+    .y-container {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      font-size: 12px;
+      color: #979797;
+      p {
+        margin-right: 34px;
+      }
+      a {
+        color: #979797;
+        margin-right: 4px;
+        font-size: 13px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/user/Login.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="login">
+    <!-- 使用el的布局容器 -->
+    <div class="main">
+      <el-row class="row">
+        <!-- 在el中栅格为24 -->
+        <el-col :span="12" class="left">
+          <div>
+            <el-carousel height="600px">
+              <el-carousel-item v-for="item in 3" :key="item">
+                <div :class="['carousel-img','item'+item]">
+                  <div>
+                    <p>钟情</p>
+                    <p>思</p>
+                  </div>
+                </div>
+                <img src="@/assets/logo.png" alt />
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </el-col>
+        <el-col :span="12" class="right">
+          <div>
+            <el-form label-width="80px">
+              
+                <h2>登陆</h2>
+             
+              <el-form-item label="用户名:">
+                <el-input type="text" placeholder="请输入用户名" v-model="val1"/>
+              </el-form-item>
+              <el-form-item label="密码:">
+                <el-input type="password" placeholder="请输入密码"  v-model="val2"/>
+              </el-form-item>
+              <el-form-item>
+                <el-Button style="width:100%" type="primary" @click="login">登陆</el-Button>
+              </el-form-item>
+              <el-form-item>
+                <div style="text-align:center">
+                  <el-button type="text">没有账号?立即注册</el-button>
+                </div>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <div class="footer">
+      <p>牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮</p>
+      <p>牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛</p>皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮牛皮
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      val1:"",
+      val2:"",
+    }
+  },
+  methods: {
+    login(){
+      //  使用路由跳转
+      this.$router.push('/');
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+/* 使用sass css预处理器 因为现在sass改名字了 scss*/
+/* 使用scss的高级css语法  */
+.login {
+  background-color: rgb(237, 237, 237);
+  min-height: 100%;
+  //设置父元素为flex布局
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  padding-top: 50px;
+  .main {
+    //等同于 .login .mian
+    width: 1100px;
+    margin: 0 auto;
+    //设置最小高度
+    min-height: 600px;
+    border: 1px solid #ccc;
+    //设置阴影 x y 半径 颜色
+    box-shadow: 1px 1px 10px #ccc, -1px -1px 10px #ccc;
+    border-radius: 4px;
+    //忽略边框
+    box-sizing: border-box;
+    .row {
+      height: 100%;
+      .left,
+      .right {
+        height: 600px;
+      }
+      .left {
+        & > div {
+          position: relative;
+          img {
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            top: 40px;
+            left: 40px;
+            z-index: 99;
+          }
+          .carousel-img {
+            height: 100%;
+            background-size: cover;
+            //  设置图片大小
+            //  设置背景图片居中显示
+            &.item1 {
+              // .carousel-img.item1
+              background-image: url("https://account.djicdn.com/pc/static/img/1.e61fc72.jpg");
+            }
+            &.item2 {
+              background-image: url("https://account.djicdn.com/pc/static/img/2.643c4c1.jpg");
+            }
+            &.item3 {
+              background-image: url("https://account.djicdn.com/pc/static/img/3.61f61fa.jpg");
+            }
+            div {
+              position: absolute;
+              right: 40px;
+              bottom: 100px;
+
+              p {
+                color: #fff;
+                font-size: 28px;
+                font-weight: 600;
+                line-height: 1.3;
+                text-align: right;
+              }
+              p:last-child {
+                font-size: 14px;
+                margin-top: 5px;
+              }
+            }
+          }
+        }
+      }
+
+      .right {
+        background-color: rgb(255, 255, 255);
+        & > div {
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          form {
+            width: 70%;
+
+            h2 {
+              text-align: center;
+              font-size: 30px;
+              font-weight: bold;
+              margin: 45px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .footer {
+    color: #fff;
+    font-size: 12px;
+    //设置字体居中
+    text-align: center;
+    line-height: 2;
+  }
+}
+</style>
